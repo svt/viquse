d23fbc41d0535dbcfadd1a947cf17030ff325559
Gustav Grusell <gustav.grusell@svt.se>
2020-08-10 15:05:21 +0200

    Add mapping for job result

---
+++ src/main/kotlin/se/svt/oss/viquse/model/vmaf/JobResult.kt
@@ -0,0 +1,28 @@
+package se.svt.oss.viquse.model.vmaf
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class JobResult(
+        val version: String,
+        val params: Map<String, Any>,
+        val metrics: List<String>,
+        val frames: List<Frame>,
+        @JsonProperty("VMAF score")
+        val vmafScore: Double,
+        @JsonProperty("ExecFps")
+        val execFps: Double
+        ) {
+}
+
+data class Frame(
+        val frameNum: Int,
+        val metrics: Metrics)
+
+data class Metrics(
+        val adm2: Double,
+        val motion2: Double,
+        val vif_scale0: Double,
+        val vif_scale1: Double,
+        val vif_scale2: Double,
+        val vmaf: Double
+)
